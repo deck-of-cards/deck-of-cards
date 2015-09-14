66019f7ebbf1d77141cd45c24c64ae1c4224b648
Juha Lindstedt <juha@pakastin.fi>
2015-09-14 08:32:41 +0300

    #16

--- dist/deck.js
+++ dist/deck.js
@@ -46,26 +46,31 @@ var Deck = (function () {
     }
   }
 
-  var transform = prefix('transform');
-  var $p = document.createElement('p');
-
-  document.body.appendChild($p);
-
-  $p.style[transform] = 'translate3d(1px,1px,1px)';
-
-  var has3d = $p.style[transform];
-
-  has3d = has3d != null && has3d.length && has3d !== 'none';
-
-  document.body.removeChild($p);
+  var has3d;
 
   function translate(a, b, c) {
+    typeof has3d !== 'undefined' || (has3d = check3d());
+
     c = c || 0;
+
     if (has3d) {
       return 'translate3d(' + a + ', ' + b + ', ' + c + ')';
     } else {
       return 'translate(' + a + ', ' + b + ')';
     }
+  }
+
+  function check3d() {
+    var transform = prefix('transform');
+    var $p = document.createElement('p');
+
+    document.body.appendChild($p);
+    $p.style[transform] = 'translate3d(1px,1px,1px)';
+
+    has3d = $p.style[transform];
+    has3d = has3d != null && has3d.length && has3d !== 'none';
+
+    document.body.removeChild($p);
   }
 
   var sort = {
