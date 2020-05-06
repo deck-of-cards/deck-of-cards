8e5c4ad3a615d875fcd894e37a89d1ea3ac110c9
Juha Lindstedt <juha@pakastin.fi>
2020-05-07 01:11:53 +0300

    Refactor

--- example.js
+++ example.js
@@ -1,28 +1,32 @@
-import {
-  Game,
-  Deck,
-  Renderer
-} from './lib/index.js';
+import { Game, AnimatedDeck, Renderer } from './lib/index.js';
+import { standardDeck } from '../standard-deck/dist/index.js';
 
-import {
-  standardDeck
-} from '../standard-deck/dist/index.js';
-
-const game = new Game({
+const gameSettings = {
   width: 1920,
-  height: 1080
-});
+  height: 1080,
+  cardTypes: {
+    standard: standardDeck
+  }
+};
 
-const deck = new Deck({
+const deckSettings = {
   x: 1920 / 2,
   y: 1080 / 2
+};
+
+const game = new Game(gameSettings);
+const deck = new AnimatedDeck(deckSettings);
+
+const standardCards = game.createCards({
+  type: 'standard',
+  start: 0,
+  end: 52
+}, {
+  side: 'back'
 });
 
 game.add(deck);
-
-deck.createCards(54, standardDeck, {
-  side: 'back'
-});
+deck.add(standardCards);
 
 const $game = document.querySelector('#game');
 const renderer = new Renderer(game, $game);
